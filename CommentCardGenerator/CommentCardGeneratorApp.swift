d1bd6964c8b1874a84feab7ba77d96bf6200d71c
David Jin Li <jin_li.d@etoncollege.org.uk>
2022-02-08 11:20:29 +0000

    MVP

--- CommentCardGenerator/CommentCardGeneratorApp.swift
+++ CommentCardGenerator/CommentCardGeneratorApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CommentCardGeneratorApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(isHappy: true, isEnjoyable: true)
         }
     }
 }
--- CommentCardGenerator/ContentView.swift
+++ CommentCardGenerator/ContentView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isHappy: Bool
+    @State var isEnjoyable: Bool
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form {
+            VStack {
+                if isHappy {
+                    Button("I am: 😁", action: {toggleHappiness()})
+                        
+                } else {
+                    Button("I am: ☹️", action: {toggleHappiness()})
+                }
+            }
+            Text("I am feeling \(isHappy ? "confident" : "worried") in Computer Science")
+                .padding()
+        }
+        
+    }
+    
+    func toggleHappiness() {
+        isHappy.toggle()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(isHappy: true, isEnjoyable: true)
     }
 }
