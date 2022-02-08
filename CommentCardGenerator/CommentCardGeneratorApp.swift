a4aef4657635d3071c9cc443e56ab3fb915befa6
David Jin Li <jin_li.d@etoncollege.org.uk>
2022-02-08 10:51:54 +0000

    Initial Commit

---
+++ CommentCardGenerator/CommentCardGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommentCardGeneratorApp.swift
+//  CommentCardGenerator
+//
+//  Created by Jin Li, David (MAG) on 08/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct CommentCardGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CommentCardGeneratorUITests/CommentCardGeneratorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CommentCardGeneratorUITestsLaunchTests.swift
+//  CommentCardGeneratorUITests
+//
+//  Created by Jin Li, David (MAG) on 08/02/2022.
+//
+
+import XCTest
+
+class CommentCardGeneratorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
